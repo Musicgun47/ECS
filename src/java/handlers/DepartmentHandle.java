08b5ba887ee53c491e8b647e68fcfecfbdea8a9d
Joel Fortier <musicgun47@gmail.com>
2016-10-21 02:34:13 +1030

    Beans for handling departments

---
+++ src/java/handlers/DepartmentHandle.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package handlers;
+
+/**
+ *
+ * @author JOEL
+ */
+public class DepartmentHandle {
+    
+    private String department;
+    
+    public DepartmentHandle(){
+        department = null;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
