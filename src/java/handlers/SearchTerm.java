f33ed2382538e3a06f8c408922015ccf08b46a18
Joel Fortier <musicgun47@gmail.com>
2016-10-21 17:04:48 +1030

    search term handler

---
+++ src/java/handlers/SearchTerm.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package handlers;
+
+/**
+ *
+ * @author JOEL
+ */
+public class SearchTerm {
+    
+    private String searchTerm;
+    
+    public SearchTerm(){
+        searchTerm = "";
+    }
+
+    public String getSearchTerm() {
+        return searchTerm;
+    }
+
+    public void setSearchTerm(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
+}
